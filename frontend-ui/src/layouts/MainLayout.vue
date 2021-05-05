22091521525b7c6141ce5afe7533dacba2722e2a
Joachim-Wambua <j.wambua@alustudent.com>
2021-05-05 02:23:18 +0200

    first commit

---
+++ frontend-ui/src/layouts/MainLayout.vue
@@ -0,0 +1,144 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header class="bg-white text-black">
+      <q-toolbar class="logo">
+        <q-toolbar-title >
+          <q-avatar square>
+            <img class="site_logo" src="../statics/proom_logo.png">
+          </q-avatar>
+        Proom
+        </q-toolbar-title>
+
+        <div class="menubar gt-sm">
+          <q-tabs v-model="tab" no-caps class="tab-text" align="start">
+            <q-route-tab to="/" label="Courses" />
+            <q-route-tab to="/" label="Features" />
+            <q-route-tab to="/" label="Support" />
+            <q-btn dense round flat icon="email" class="email_button">
+              <q-badge color="red" floating transparent >
+                3
+              </q-badge>
+            </q-btn>
+            <q-avatar class="profileavatar">
+              <img src="../statics/profile_avatar.jpg">
+            </q-avatar>
+          </q-tabs>
+      </div>
+
+      <div class="lt-md">
+        <q-btn flat 
+          dense 
+          round 
+          @click="leftDrawerOpen=!leftDrawerOpen"
+          icon = "menu" aria-label="Menu">
+          
+        </q-btn>
+      </div>
+      </q-toolbar>
+
+      
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label header>Menu</q-item-label>
+        <q-item clickable tag="a" target="_blank" rel="noopener" href="">
+          <q-item-section avatar>
+            <q-icon name="school" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Courses</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" rel="noopener" href="">
+          <q-item-section avatar>
+            <q-icon name="code" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Features</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" rel="noopener" href="">
+          <q-item-section avatar>
+            <q-icon name="chat" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Support</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <!-- <q-footer reveal elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer> -->
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
+
+<style>
+
+.logo{
+  padding-left: 120px;
+  font-family: Montserrat Black;
+  size: 24px;
+}
+
+.tab-text{
+  color: rgb(151, 151, 151);
+  text-transform: lowercase;
+  font-family: Montserrat-Medium;
+}
+
+.email_button{
+ color: rgb(80, 80, 80);
+ display: flex;
+}
+
+.menubar{
+  padding-right: 120px;
+  display: flex;
+  align-self: center;
+  justify-content: center;
+  float: left;
+  
+}
+
+.profileavatar{
+  padding-left: 4px;
+  size: 30px;
+
+}
+
+.site_logo{
+  size: 18px;
+  shape-outside: padding-box;
+}
+
+</style>
---
+++ frontend-ui/src/pages/Index.vue
@@ -0,0 +1,104 @@
+<template>
+ <div class="q-pa-md">
+   <!-- Define the row -->
+   <div class="row align-center">
+     <!-- The class below makes the Layout Responsive -->
+ <div padding class="col-12 col-md" >
+     <div class="pagetitle text-h5">Study different areas of web 
+       design easily in 3 steps:</div>
+    <div ><p padding class="bullettext"><span padding class="bulletnumber">.01</span> Visit our website</p></div>
+    <div ><p padding class="bullettext"><span padding class="bulletnumber">.02</span> Sign up</p></div>
+    <div ><p padding class="bullettext"><span padding class="bulletnumber">.03</span> Find the course you want</p></div>
+    <div class="signindiv"><q-btn class="signinbutton" no-caps label="Sign In"/></div>
+ </div>
+ 
+ <div padding class=" col-12 col-md">
+   <!-- Card Section Column -->
+   <q-card class="my-card text-white">
+      <div class="q-pa-md">
+          <q-img class="main_img" src="../statics/stationery_image.png"
+          />
+        </div>
+      <!-- <div class="text-h6">Our Changing Planet</div> -->
+    </q-card>
+ </div>
+   </div>
+ </div>
+
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
+
+
+<style lang="sass" scoped>
+.row > div
+  display: block
+  padding: 10px 15px
+  border: 1px solid #fff
+.row + .row
+  margin-top: 1rem
+
+.column > div
+  padding: 10px
+
+.my-card
+  padding: 0px
+  background-color: #80c2ff
+  width: 95%
+  height:95%
+  justify-content: center
+  align-content: center
+  border-radius: 36px
+  
+.main_img
+  padding: 10px
+  justify-content: center
+  align-content: center
+  size: 150px
+  
+.pagetitle
+  padding-top: 90px 
+  padding-bottom: 75px
+  padding-left: 90px
+  width: 75%
+  display: inline-block
+  font-family: "Montserrat ExtraBold"
+  word-wrap: break-word
+  overflow-wrap: break-word
+  text-justify: left
+  font-size: 36px
+  line-height: 45px
+
+
+.bulletnumber
+  font-family: Montserrat SemiBold
+  font-size: 12px
+  font-weight: bolder
+  color: #839eff
+  padding-left: 90px
+  padding-right: 15px
+
+.bullettext
+  padding-bottom: 15px
+  font-family: Montserrat Medium
+  font-size: 18px
+   
+
+.signinbutton
+  background-color: #839eff
+  color: #fff
+  padding: 10px
+  padding-left: 45px
+  font-family: Montserrat Semibold
+  padding-right: 45px 
+  border-radius: 45px
+  box-shadow: #839eff
+  
+.signindiv
+  padding-left: 90px
+
+</style>
